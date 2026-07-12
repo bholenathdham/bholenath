95c148a78219719e7e803a483233e1210495aa3d
bholenathdham <rohannindia@gmail.com>
2026-07-12 14:14:25 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,28 @@
+const toggle = document.getElementById("langToggle");
 
+let bengali = false;
+
+toggle.addEventListener("click",()=>{
+
+const en = document.querySelectorAll(".en");
+const bn = document.querySelectorAll(".bn");
+
+bengali = !bengali;
+
+if(bengali){
+
+en.forEach(el=>el.classList.add("hidden"));
+bn.forEach(el=>el.classList.remove("hidden"));
+
+toggle.innerText="English";
+
+}else{
+
+en.forEach(el=>el.classList.remove("hidden"));
+bn.forEach(el=>el.classList.add("hidden"));
+
+toggle.innerText="বাংলা";
+
+}
+
+});
