0a827eb6e7ae36ba4849298888460d27696fdf1e
bholenathdham <rohannindia@gmail.com>
2026-07-12 18:20:55 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -35,3 +35,36 @@ menuToggle.addEventListener("click", () => {
     navbar.classList.toggle("active");
 
 });
+
+const galleryImages = document.querySelectorAll(".gallery img");
+const lightbox = document.getElementById("lightbox");
+const lightboxImg = document.getElementById("lightbox-img");
+const closeLightbox = document.querySelector(".close-lightbox");
+
+galleryImages.forEach(img => {
+
+    img.addEventListener("click", () => {
+
+        lightbox.style.display = "flex";
+
+        lightboxImg.src = img.src;
+
+    });
+
+});
+
+closeLightbox.addEventListener("click", () => {
+
+    lightbox.style.display = "none";
+
+});
+
+lightbox.addEventListener("click", (e) => {
+
+    if(e.target === lightbox){
+
+        lightbox.style.display = "none";
+
+    }
+
+});
