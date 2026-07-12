7d5a0c2b76000e2c7b3c434eb48cc962e8b321b3
bholenathdham <rohannindia@gmail.com>
2026-07-12 17:29:14 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -26,3 +26,12 @@ toggle.innerText="বাংলা";
 }
 
 });
+
+const menuToggle = document.getElementById("menuToggle");
+const navbar = document.getElementById("navbar");
+
+menuToggle.addEventListener("click", () => {
+
+    navbar.classList.toggle("active");
+
+});
