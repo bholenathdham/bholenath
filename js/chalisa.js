0b20ccc2d733b4c50201418e994a85d6488ff2df
bholenathdham <rohannindia@gmail.com>
2026-07-14 04:44:47 +0530

    Update chalisa.js

--- js/chalisa.js
+++ js/chalisa.js
@@ -1,1 +1,28 @@
+const buttons = document.querySelectorAll(".lang-btn");
+const contents = document.querySelectorAll(".lang-content");
 
+buttons.forEach(button => {
+
+button.addEventListener("click", () => {
+
+const lang = button.dataset.lang;
+
+buttons.forEach(btn =>
+btn.classList.remove("active")
+);
+
+button.classList.add("active");
+
+contents.forEach(content => {
+
+content.classList.remove("active");
+
+if(content.classList.contains(lang)){
+content.classList.add("active");
+}
+
+});
+
+});
+
+});
