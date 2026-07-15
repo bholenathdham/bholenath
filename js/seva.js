b093e566e994d527a02dab20d4c20243cad419f5
bholenathdham <rohannindia@gmail.com>
2026-07-15 23:39:13 +0530

    Update seva.js

--- js/seva.js
+++ js/seva.js
@@ -1,1 +1,31 @@
+document.addEventListener("DOMContentLoaded", () => {
 
+    const langBtn = document.getElementById("langToggle");
+
+    let currentLang = "en";
+
+    langBtn.addEventListener("click", () => {
+
+        const enElements = document.querySelectorAll(".en");
+        const bnElements = document.querySelectorAll(".bn");
+
+        if (currentLang === "en") {
+
+            enElements.forEach(el => el.classList.add("hidden"));
+            bnElements.forEach(el => el.classList.remove("hidden"));
+
+            langBtn.textContent = "English";
+            currentLang = "bn";
+
+        } else {
+
+            bnElements.forEach(el => el.classList.add("hidden"));
+            enElements.forEach(el => el.classList.remove("hidden"));
+
+            langBtn.textContent = "বাংলা";
+            currentLang = "en";
+        }
+
+    });
+
+});
