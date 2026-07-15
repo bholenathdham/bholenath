28f48bc05e5d394b79dc51c42fa5872df018cc41
bholenathdham <rohannindia@gmail.com>
2026-07-15 13:47:37 +0530

    Update 108names.js

--- js/108names.js
+++ js/108names.js
@@ -1,1 +1,28 @@
+const langButtons = document.querySelectorAll('.lang-btn');
+const allLanguages = document.querySelectorAll('.name-lang');
 
+langButtons.forEach(button => {
+
+button.addEventListener('click', () => {
+
+langButtons.forEach(btn =>
+btn.classList.remove('active')
+);
+
+button.classList.add('active');
+
+const selectedLang = button.dataset.lang;
+
+allLanguages.forEach(item =>
+item.classList.remove('active')
+);
+
+document
+.querySelectorAll('.name-lang.' + selectedLang)
+.forEach(item =>
+item.classList.add('active')
+);
+
+});
+
+});
